bcc1517ee6f5ba0a9711a5ef757259d898f9804e
101415 <j.chicharito.h.0014@gmail.com>
2020-01-09 18:37:08 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock '3.11.2'
 
 # Capistranoのログの表示に利用する
-set :application, 'chat-spce'
+set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:101415/chat-space.git'
